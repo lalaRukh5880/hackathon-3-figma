deffb7d2856fd425f8c27f9b8ca5a2ce9c60d147
lalaRukh5880 <160532373+lalaRukh5880@users.noreply.github.com>
2025-01-22 15:14:19 +0500

    some chagnes

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      domains: ['cdn.sanity.io'],
-    },
-  };
-  
-   export default nextConfig;
+  reactStrictMode: true, // Enables React's Strict Mode for better error handling
+  swcMinify: true, // Enables SWC-based minification
+  compiler: {
+      styledComponents: true, // Enables styled-components SWC transform
+  },
+  webpack(config, { isServer }) {
+      // Customize Webpack config if needed
+      return config;
+  },
+  images: {
+      domains: ['cdn.sanity.io'], // Allow images from specified domains
+  },
+};
+
+export default nextConfig;
