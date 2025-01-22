12833b881c988d6852458bee2183ca2e0ddf2b2a
lalaRukh5880 <160532373+lalaRukh5880@users.noreply.github.com>
2025-01-22 14:49:50 +0500

    changes...

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,7 +7,6 @@ const config: Config = {
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-	'./@/**/*.{ts,tsx}', 
   ],
   theme: {
   	extend: {}
