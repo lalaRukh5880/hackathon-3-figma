ca37f295f8d6a1c5dfab857571f1f0d770b07cd5
lalaRukh5880 <160532373+lalaRukh5880@users.noreply.github.com>
2025-01-22 00:37:49 -0800

    again changes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -94,6 +94,6 @@ const config: Config = {
   		}
   	}
   },
-  plugins: [require("@tailwindcss/forms"), require("tailwindcss-animate")],
+  plugins: [require("tailwindcss-animate")],
 };
 export default config;
